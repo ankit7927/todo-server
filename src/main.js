7fdb052d185bdf8c68fd0307003d4516b677e52d
ankit <ap8424856@gmail.com>
2024-09-24 18:52:31 +0530

    added cors

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./configs/config.db");
 const mongoose = require("mongoose");
-
+const cors = require("cors")
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }));
 
