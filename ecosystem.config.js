ded04f7f1b7f2d116b9e6fa42733e1aac9b9185e
ankit <ap8424856@gmail.com>
2024-09-25 11:32:56 +0530

    added env var in eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,14 @@
 module.exports = {
 	apps: [
 		{
-      name: "todo server",
+			name: "todo server",
 			script: "src/main.js",
+			env: {
+				NODE_ENV: process.env.NODE_ENV,
+				STRIPE_SECRETE: process.env.STRIPE_SECRETE,
+				DB_USERNAME: process.env.DB_USERNAME,
+				DB_PASSWORD: process.env.DB_PASSWORD,
+			},
 		},
 	],
 };
