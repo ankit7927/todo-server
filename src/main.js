fac5f918d06a602dafc51ae91ec273e651612a72
ankit <ap8424856@gmail.com>
2024-09-25 12:30:06 +0530

    all origin alloed

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors({ origin: ["*"] }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }));
 
