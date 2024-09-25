7296353118f16be999f3278588747f6308418487
ankit <ap8424856@gmail.com>
2024-09-25 11:05:02 +0530

    updated db config

--- src/configs/config.db.js
+++ src/configs/config.db.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-let db_url = ""
-
-if (process.env.NODE_ENV === "pro") db_url = process.env.REMOTE_DATABASE_URL || "";
+let db_url = "";
+if (process.env.NODE_ENV === "pro")
+	db_url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@todo-cluster.iiol7.mongodb.net/?retryWrites=true&w=majority&appName=todo-cluster`;
 else db_url = "mongodb://127.0.0.1:27017/todo";
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db_url)
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		await mongoose.connect(db_url);
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 module.exports = connectDB;
